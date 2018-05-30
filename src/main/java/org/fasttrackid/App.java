641ad37428e62b283b4a85714578eda852b88238
PatriBura <patriciabura91@gmail.com>
2018-05-30 10:51:34 +0300

    added new log

--- src/main/java/org/fasttrackid/App.java
+++ src/main/java/org/fasttrackid/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "Hello darkness my old friend..." );
+        System.out.println( "I've come to talk to you again..." );
     }
 }
