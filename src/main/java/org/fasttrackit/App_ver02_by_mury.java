f5edd398c7821e36c42590244a4747b8d0637c7c
PatriBura <patriciabura91@gmail.com>
2018-05-30 13:58:32 +0300

    added new log in main method

--- src/main/java/org/fasttrackit/App_ver02_by_mury.java
+++ src/main/java/org/fasttrackit/App_ver02_by_mury.java
@@ -11,6 +11,9 @@ public class App_ver02_by_mury
             System.out.println( "And the vision that was planted in my brain" );
             System.out.println( "Still remains" );
             System.out.println( "Within the sound of silence" );
+            System.out.println( "................" );
+            System.out.println( "................" );
+            System.out.println( "................" );
         }
 
     }
